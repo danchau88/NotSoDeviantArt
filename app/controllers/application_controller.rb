5efaac69579555fc59fbb27be52e1f4ba18cd30b
Daniel Chau <danchau88@gmail.com>
2020-06-22 21:12:07 -0700

    updated Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,30 @@
 class ApplicationController < ActionController::Base
-    
+    helper_method :current_user
+
+    def current_user
+        @current_user = User.find_by(session_token: session[:session_token])
+    end
+
+    def require_login
+        redirect_to new_api_session_url if !logged_in?
+    end
+
+    def login(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout
+        current_user.reset_session_token!
+        @current_user = nil 
+        session[:session_token] = nil if logged_in?
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
