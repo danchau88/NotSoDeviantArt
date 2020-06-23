b90244d78d4dfff6119fa0f8707b4a501b66e9fa
Daniel Chau <danchau88@gmail.com>
2020-06-23 11:02:44 -0700

    setup actions, reducers, store, components(root and App)

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,39 @@
+import {postUser, postSession, deleteSession} from '../util/session_api_util'
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER"
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER"
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS"
+
+
+export const receiveCurrentUser = (user) =>{
+    return {
+        type: RECEIVE_CURRENT_USER ,
+        user: user
+    }
+}
+
+export const logoutCurrentUser = () => {
+    return {
+        type: LOGOUT_CURRENT_USER
+    }
+};
+
+export const receiveErrors = (errors) => {
+    return {
+        type: RECEIVE_SESSION_ERRORS,
+        errors: errors
+    }
+}
+
+export const login = (user) => (dispatch) => postSession(user)
+    .then( res=> dispatch(receiveCurrentUser(res)))
+    .fail((errors) => dispatch(receiveErrors(errors.responseJSON))); 
+
+
+export const signup = (user) => (dispatch) => postUser(user)
+    .then(res => dispatch(receiveCurrentUser(res)))
+    .fail((errors) => dispatch(receiveErrors(errors.responseJSON))); 
+
+export const logout = () => (dispatch) => deleteSession()
+    .then(() => dispatch(logoutCurrentUser()))
+    .fail((errors) => dispatch(receiveErrors(errors.responseJSON))); 
--- frontend/not_so_deviant_art.jsx
+++ frontend/not_so_deviant_art.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {postUser, postSession, deleteSession} from "./util/session_api_util";
+import configureStore from "./store/store";
+import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>NotSoDeviantArt</h1>, root);
+  const store = configureStore();
+
+  // TEST START
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  // TEST END
+
+  ReactDOM.render(<Root store={store} />, root);
+
 });
+
+// TEST UTILS
+window.postUser = postUser;
+window.postSession = postSession;
+window.deleteSession = deleteSession;
+
