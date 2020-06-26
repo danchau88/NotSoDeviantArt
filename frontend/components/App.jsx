6f57cda52502375c5bca0a8b7a75d32d3d06b29c
Daniel Chau <danchau88@gmail.com>
2020-06-25 20:00:25 -0700

    just a little more CSS styling on the dropdown, added the HOME button back

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -8,22 +8,28 @@ import {AuthRoute} from './../util/route_util';
 const App = () => (
   <div className='navbar'>
     <header className='nav-left'> 
-        <Link to='/'>
-          <h1>NotSo</h1>
-          <i className="fab fa-deviantart"></i>
-          <h1>DeviantArt</h1>
-        </Link>
-        <button className='search-button'>
-          <i className="fas fa-search"></i>
-        {' SEARCH'}</button>
-        <div className='dropdown'>
-          <button className="nav-button" id='more'>MORE</button>
-          <div className='dropdown-content'>
-            <button>WATCH</button>
-            <button>FAVORITE</button>
-            <button>EXPLORE</button>
-          </div>
+      <Link to='/'>
+        <h1>NotSo</h1>
+        <i className="fab fa-deviantart"></i>
+        <h1>DeviantArt</h1>
+      </Link>
+
+      <Link id='home-btn' to='/'>HOME</Link>
+
+      <div className='dropdown'>
+        <button className="nav-button" id='more'>MORE</button>
+        <div className='dropdown-content'>
+          <button>WATCH</button>
+          <button>FAVORITE</button>
+          <button>EXPLORE</button>
         </div>
+      </div>
+
+      <button className='search-button'>
+        <i className="fas fa-search"></i>
+        {' SEARCH'}
+      </button>
+
     </header> 
 
     <div className='nav-right'>
