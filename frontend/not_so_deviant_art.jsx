d97f752e3a598b3f1459aa85b20c4423d7c84339
Daniel Chau <danchau88@gmail.com>
2020-08-03 15:06:29 -0700

    took out the the testing windows functions for comments in not_so_deviant_art.jsx

--- frontend/not_so_deviant_art.jsx
+++ frontend/not_so_deviant_art.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
 import Root from "./components/root";
-import {getAllComments, createComment} from './actions/comments_actions';
-import { fetchAllComments, postComment } from './util/comments_api_util';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
@@ -20,14 +18,6 @@ document.addEventListener("DOMContentLoaded", () => {
   } else {
     store = configureStore();
   }
-
-  // TESTING COMMENTS REMOVE LATER
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.createComment = createComment;
-  window.getAllComments = getAllComments;
-  window.postComment = postComment;
-  window.fetchAllComments = fetchAllComments;
 
   ReactDOM.render(<Root store={store} />, root);
 
